c98258ce23c76c7ff81100d072bfe5b0298f6e56
apb21 <andrebritton@hotmail.com>
2017-02-09 18:56:34 +0000

    Improved error handling and added GetInt and CheckInt functions.

--- SumAny.c
+++ SumAny.c
@@ -1,22 +1,66 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+int failure()
+{
+  printf("That was not a valid Integer. Program closing.\n");
+  exit(0);
+}
+
+int CheckInt(char *X) {
+  int IsInteger = TRUE;
+  if (strlen(X)>10) {
+    IsInteger = FALSE;
+    return IsInteger;
+  }
+  for (int i = 0; i < strlen(X);i++)
+  {
+    if(X[i] < '0' || X[i] > '9')
+    {
+      IsInteger = FALSE;
+      break;
+    }
+  }
+  return IsInteger;
+}
+
+int GetInt(){
+  char InputString[10];
+  scanf("%s", InputString);
+  if (CheckInt(InputString) == TRUE) {
+    return atoi(InputString);
+  } else {
+    failure();
+  }
+}
+
 int main()
 {
-  printf("This program will return the sum of all integers between two integers (inclusive).\n");
-  int NUMB1, NUMB2;
+  printf("This program will return the sum of all integers between two integers (inclusively) as a float.\n");
+  printf("N.B. The maximum length of an integer is 10 digits.\n");
+  int Int1, Int2;
   printf("Input first integer.\n");
-  scanf("%d", &NUMB1);
+  Int1 = GetInt();
   printf("Input second integer.\n");
-  scanf("%d", &NUMB2);
-  if (NUMB1>NUMB2) {
-    int SWAP;
-    SWAP = NUMB1;
-    NUMB1 = NUMB2;
-    NUMB2 = SWAP;
+  Int2 = GetInt()+1;
+  if (Int1>Int2) {
+    int Swap;
+    Swap = Int1;
+    Int1 = Int2;
+    Int2 = Swap;
   }
-  int TOTAL = 0;
-  do{
-    TOTAL = TOTAL + NUMB1;
-    NUMB1 = NUMB1 + 1;
-  } while(NUMB1 < (NUMB2 + 1));
-  printf("%d\n", TOTAL);
+  float Total = 0;
+  for (int i = Int1; i < Int2; i++) {
+    Total = Total + i;
+  }
+  printf("%f\n", Total);
 }
